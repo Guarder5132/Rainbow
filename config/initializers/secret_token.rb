84c32340bf7fbe457d4ee28a2c0df63c58ee2007
Guarder <Guarder@example.com>
2019-03-04 10:51:03 +0800

    新项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainbow::Application.config.secret_key_base = '157e96453f0bfb82d3bd99ef0321f1926025fbe2040fbb6794033a13e6212df5ed935df443cd0de2086c933ab34deca019948481f5dca2f21b7d71ed5c30980d'
